86222d37466f4f8118f50be5104a97ee41c5debc
ChuluM <zanakason.cm@gmail.com>
2023-06-18 17:16:18 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,12 +8,14 @@
 int main(void)
 {
 char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        int i;
+{
+	int i;
 
 	for (i = 0; i < 52; i++)
-	{
-		putchar(alp[i]);
-	}
-	putchar('\n');
-	return (0);
+        {
+                putchar(alp[i]);
+        }
+        putchar('\n');
+        return (0);
+}
 }
